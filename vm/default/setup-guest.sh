88d2d45e5f65d1309c808c924ca46f636b0ec65d
Timothy Washington <twashing@gmail.com>
2014-06-01 18:36:46 -0400

    -- adding more debug space

--- vm/default/setup-guest.sh
+++ vm/default/setup-guest.sh
@@ -4,36 +4,36 @@
 sudo apt-get update && 
 
 # Tools 
-echo "Install General Tools"
+echo; echo "Install General Tools"
 sudo apt-get -y install tree tmux &&
 
 # Vim 
-echo "Installing Git..."
+echo; echo "Installing Git..."
 sudo apt-get install -y vim-nox &&
 
 # Emacs (https://launchpad.net/~cassou/+archive/emacs)
-echo "Installing Vim..."
+echo; echo "Installing Vim..."
 sudo apt-get install -y software-properties-common python-software-properties && 
 sudo add-apt-repository ppa:cassou/emacs && sudo apt-get update && sudo apt-get install -y emacs24 &&
 
 # Emacs Live
-echo "Installing Emacs..."
+echo; echo "Installing Emacs..."
 bash <(curl -fksSL https://raw.github.com/overtone/emacs-live/master/installer/install-emacs-live.sh) &&
 
 # Git 
-echo "Installing Emacs-Live..."
+echo; echo "Installing Emacs-Live..."
 sudo apt-get -y install libcurl4-gnutls-dev libexpat1-dev gettext libz-dev libssl-dev &&
 sudo apt-get -y install git &&
 
 # Java 
-echo "Installing Java..."
+echo; echo "Installing Java..."
 sudo apt-get -y install python-software-properties &&
 sudo add-apt-repository -y ppa:webupd8team/java &&
 sudo apt-get update &&
 sudo apt-get -y install oracle-java7-installer &&
 
 # Leiningen
-echo "Installing Leiningen..."
+echo; echo "Installing Leiningen..."
 mkdir ~/bin &&
 wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein &&
 chmod u+x ~/bin/lein &&
@@ -41,6 +41,7 @@ chmod u+x ~/bin/lein &&
 
 
 # Configurations
+echo; echo "Setting Configurations..."
 mkdir Projects &&
 git clone https://github.com/twashing/configurations.git Projects/configurations &&
 link -s Projects/configurations/bash_profile ~/.bash_profile &&
