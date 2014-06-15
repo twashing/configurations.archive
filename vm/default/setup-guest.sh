e4b11e1574566b121004caacfd2aad5b552d6693
Timothy Washington <twashing@gmail.com>
2014-06-15 08:32:06 -0400

    -- commenting uneeded

--- vm/default/setup-guest.sh
+++ vm/default/setup-guest.sh
@@ -50,6 +50,8 @@ sudo apt-get -y install git &&
 
 # Java 
 echo; echo ">> Installing Java..."
+# sudo apt-get update &&
+# sudo apt-key update
 sudo apt-get -y install python-software-properties &&
 sudo add-apt-repository -y ppa:webupd8team/java &&
 sudo apt-get update &&
