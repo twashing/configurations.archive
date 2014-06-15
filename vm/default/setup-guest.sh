2442da336eda8c7a97d23b335f17931b9758812c
Timothy Washington <twashing@gmail.com>
2014-06-14 22:20:14 -0400

    -- uneeded backups

--- vm/default/setup-guest.sh
+++ vm/default/setup-guest.sh
@@ -65,8 +65,8 @@ chmod u+x ~/bin/lein &&
 # Configurations
 echo; echo ">> Setting Configurations..."
 mv .bashrc{,.1}
-mv .bash_profile{,.1}
-mv .live-packs/$(echo $USER)-pack/init.el{,.1}  # backup old configs
+#mv .bash_profile{,.1}
+#mv .live-packs/$(echo $USER)-pack/init.el{,.1}  # backup old configs
 mkdir -p Projects &&
 git clone https://github.com/twashing/configurations.git Projects/configurations &&
 ln -s ~/Projects/configurations/bash_profile ~/.bash_profile &&
