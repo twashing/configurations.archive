bb463e5c32215fd84cceb712158450104e8f9e02
Timothy Washington <twashing@interruptsoftware.com>
2014-10-09 00:21:58 +0000

    -- cleaning up lein profiles

--- lein_profiles.clj
+++ lein_profiles.clj
@@ -8,39 +8,17 @@
             [lein-try "0.4.1"]
             [lein-localrepo "0.5.3"]
             [alembic "0.2.1"]
-            ;;[hiccup-bridge "1.0.0-SNAPSHOT"]
-            ;; [com.cemerick/austin "0.1.4"]
+            [cider/cider-nrepl "0.7.0"]
             [lein-cljsbuild "1.0.3"]
-            ;;[lein-release "1.0.5"]
             [lein-clojars "0.9.1"]]
-  :dependencies [[nrepl-inspect "0.4.1"]
-                 [ritz/ritz-nrepl-middleware "0.7.0"]
 
-                 [com.cemerick/pomegranate "0.3.0"]
-                 ;;[com.cemerick/piggieback "0.1.3"]
-                 ;;[leiningen #= (leiningen.core.main/leiningen-version)]
-                 ;;[im.chit/vinyasa "0.2.2"]
-                 ;;[org.clojars.gjahad/debug-repl "0.3.3"]
-                 ]
-  #_:injections #_[(require '[vinyasa.inject :as inj])
-               (require 'alex-and-georges.debug-repl)
-               (inj/inject 'clojure.core '>
-                           '[[alex-and-georges.debug-repl debug-repl]
+  :dependencies [[org.clojure/tools.namespace "0.2.7"]
+                 [im.chit/vinyasa "0.2.2"]
+                 [spyscope "0.1.4"]]
 
-                             [vinyasa.inject inject]
-                             [vinyasa.pull pull]
-                             [vinyasa.lein lein]
-                             [vinyasa.reimport reimport]
+  :injections [(require 'spyscope.core)
+               (require '[vinyasa.inject :as inject])
+               (inject/in [vinyasa.pull :all]
 
-                             [cemerick.pomegranate add-classpath get-classpath resources]
-                             [clojure.java.io resource]
-                             [clojure.tools.namespace.repl refresh]
-                             [clojure.repl apropos dir doc find-doc source pst
-                              [root-cause >cause]]
-
-                             [clojure.pprint pprint pp]])]
-
-  #_:repl-options #_{:nrepl-middleware
-                 [ritz.nrepl.middleware.javadoc/wrap-javadoc
-                  ritz.nrepl.middleware.simple-complete/wrap-simple-complete
-                  inspector.middleware/wrap-inspect]}}}
+                          clojure.core
+                          [iroh.core .> .? .* .% .%>])]}}
