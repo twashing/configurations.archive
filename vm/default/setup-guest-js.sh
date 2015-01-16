f03ed41aa64d2c37bf51236751fb97a1ce8dfbd8
Timothy Washington <twashing@gmail.com>
2015-01-16 14:37:36 -0500

    -- updating node installation method

--- vm/default/setup-guest-js.sh
+++ vm/default/setup-guest-js.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Node & NPM
-sudo apt-get -y install nodejs npm &&
-sudo npm install -g yo &&
+# sudo apt-get -y install nodejs npm &&
+
+# https://github.com/joyent/node/wiki/installation#building-on-linux
+# http://nodejs.org/download/
+bash < <(curl http://h3manth.com/njs)
+# sudo npm install -g yo &&
 sudo npm install -g grunt &&
 sudo npm install -g grunt-cli &&
 sudo npm install -g bower
