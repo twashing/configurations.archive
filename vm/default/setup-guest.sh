0b032588b4003fc8bdac2804952486cb8258f7da
Timothy Washington <twashing@interruptsoftware.com>
2015-01-16 01:41:11 +0000

    -- factoring out different dev environments

--- vm/default/setup-guest.sh
+++ vm/default/setup-guest.sh
@@ -49,41 +49,12 @@ sudo add-apt-repository -y ppa:webupd8team/java &&
 sudo apt-get update &&
 sudo apt-get -y install oracle-java7-installer &&
 
-# Backup old ruby lib & uninstall 
-echo; echo ">> Backing up old ruby libs & uninstall..."
-sudo mv /opt/vagrant_ruby /opt/vagrant_ruby.bak
-sudo apt-get -y remove ruby1.8 &&
-sudo apt-get -y remove ruby && 
-sudo apt-get -y remove ruby-bundler && 
-sudo apt-get -y purge ruby1.8 && 
-sudo apt-get -y purge ruby && 
-sudo apt-get -y purge ruby-bundler && 
-sudo apt-get -y autoremove --purge
-
 # Node & NPM
 sudo apt-get install nodejs npm &&
-
-# ** TODO - remove this rails dependency
-sudo apt-get install libsqlite3-dev &&
+sudo npm install -g yo grunt grunt-cli bower
 
 # Local install 
 sudo su - $username << 'EOF'
-
-# Install RBenv / Ruby-Build
-echo; echo ">> Installing Rbenv (Ruby)..."
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv &&
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_local &&
-echo 'eval "$(rbenv init -)"' >> ~/.bash_local &&
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build &&
-~/.rbenv/bin/rbenv install 2.1.5 &&
-. .bash_local &&
-rbenv global 2.1.5 &&
-
-# Bundler
-gem install bundler
-
-# Rails 
-gem install rails
 
 echo ">> Runnin local config as[ $(whoami) ]"
 
