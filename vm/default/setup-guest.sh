97120873ee8c83f00e1ed4c5722d8c51bb4c29b9
Timothy Washington <twashing@interruptsoftware.com>
2015-01-28 16:47:39 +0000

    -- adding more items to gpg troubleshooting checklist

--- vm/default/setup-guest.sh
+++ vm/default/setup-guest.sh
@@ -96,6 +96,11 @@ EOF
 # Invoking gpg-agent 
 # https://www.gnupg.org/documentation/manuals/gnupg/Invoking-GPG_002dAGENT.html 
 
+# http://brondsema.net/blog/index.php/2007/02/06/keychain_gpg_agent_pinentry_problems
+# http://v3.tronche.com/section/system-administration/solved-gpg-agent-or-pinentry-ssh-or-su-not-working-linux-unix
+# http://lists.gnupg.org/pipermail/gnupg-users/2006-May/028548.html
+# http://www.ianatkinson.net/computing/gnupg.htm
+
 # Emacs Live - do manually; the emacs-live shell is interactive, meaning we have to have a standard input available
 #echo; echo ">> Installing Emacs-Live..."
 
