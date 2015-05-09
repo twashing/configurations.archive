abf70270f46b097b94f794a447e8b38f373fb7d0
Timothy Washington <twashing@gmail.com>
2015-05-09 12:41:08 -0700

    -- tweaking setup-guest process for nixos

--- profiles/nixos/bin/setup-guest.sh
+++ profiles/nixos/bin/setup-guest.sh
@@ -1,9 +1,17 @@
 
 echo -n "Please supply a username: "
 read username
-
 export USERNAME=$username
-/home/$username/Projects/configurations/profiles/core/bin/setup-guest-postinstall.sh
 
 sudo mv /etc/nixos/configuration.nix{,.1}
 sudo ln -s /home/$username/Projects/configurations/profiles/nixos/etc/configuration.nix /etc/nixos/configuration.nix
+
+sudo nixos-rebuild switch
+
+mv /home/$username/.bashrc{,.1}
+mkdir -p /home/$username/Projects &&
+
+git clone https://github.com/twashing/configurations.git /home/$username/Projects/configurations &&
+
+/home/$username/Projects/configurations/profiles/core/bin/setup-guest-postinstall.sh
+
---
+++ profiles/nixos/bin/setup-host.sh
@@ -0,0 +1,4 @@
+
+vagrant init larryweya/nixos-14.12_64  && # https://vagrantcloud.com/larryweya/boxes/nixos-14.12_64
+vagrant up && vagrant ssh
+
