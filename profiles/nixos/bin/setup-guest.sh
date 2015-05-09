0d3f9b7edad2862b98d4d25df1dd4ac879fdc933
Timothy Washington <twashing@gmail.com>
2015-05-09 12:47:02 -0700

    -- fixed a mistake in setup ordering

--- profiles/nixos/bin/setup-guest.sh
+++ profiles/nixos/bin/setup-guest.sh
@@ -3,15 +3,15 @@ echo -n "Please supply a username: "
 read username
 export USERNAME=$username
 
-sudo mv /etc/nixos/configuration.nix{,.1}
-sudo ln -s /home/$username/Projects/configurations/profiles/nixos/etc/configuration.nix /etc/nixos/configuration.nix
-
-sudo nixos-rebuild switch
-
 mv /home/$username/.bashrc{,.1}
 mkdir -p /home/$username/Projects &&
 
 git clone https://github.com/twashing/configurations.git /home/$username/Projects/configurations &&
+
+sudo mv /etc/nixos/configuration.nix{,.1}
+sudo ln -s /home/$username/Projects/configurations/profiles/nixos/etc/configuration.nix /etc/nixos/configuration.nix
+
+sudo nixos-rebuild switch
 
 /home/$username/Projects/configurations/profiles/core/bin/setup-guest-postinstall.sh
 
