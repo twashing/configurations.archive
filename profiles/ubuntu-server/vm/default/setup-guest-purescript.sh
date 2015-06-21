0adc96f69f56e5b0e0ab60586e9174abe5c240e8
Timothy Washington <twashing@gmail.com>
2015-06-21 20:26:06 +0000

    -- adding node installation to purescript setup

--- profiles/ubuntu-server/vm/default/setup-guest-purescript.sh
+++ profiles/ubuntu-server/vm/default/setup-guest-purescript.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# install node
+
+# requires an install of GHC, GHCi and Cabal-Install
+
+curl -sL https://deb.nodesource.com/setup_0.10 | sudo bash -
+sudo apt-get install -y nodejs  # node
+
 cabal install purescript  # psci
 npm install -g pulp
 
