6e6294a424c94a29808f284720ff87d2f0c2f610
Timothy Washington <twashing@interruptsoftware.com>
2016-01-01 10:14:25 -0500

    -- cleaning up Clojure install

--- profiles/ubuntu-server/vm/default/setup-guest-clojure.sh
+++ profiles/ubuntu-server/vm/default/setup-guest-clojure.sh
@@ -4,6 +4,5 @@ echo; echo ">> Installing Leiningen..."
 mkdir ~/bin   # may already exist
 wget -O ~/bin/lein https://raw.github.com/technomancy/leiningen/stable/bin/lein &&
 chmod u+x ~/bin/lein &&
-~/bin/lein
 
-ln -s ~/Projects/configurations/lein_profiles.clj ~/.lein/profiles.clj &&
+ln -s ~/Projects/configurations/lein_profiles.clj ~/.lein/profiles.clj
