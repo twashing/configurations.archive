b947598b64d76f6f78957e8288b6e89abc5d5ac7
Timothy Washington <twashing@interruptsoftware.com>
2016-01-01 01:52:37 -0500

    -- removing unused profile; updating powerline shell config

--- profiles/ubuntu-server/vm/default/setup-guest-purescript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# requires an install of GHC, GHCi and Cabal-Install
-# requires an install of NodeJS
-
-cabal install purescript  # psci
-npm install -g pulp
-
--- profiles/ubuntu-server/vm/default/setup-guest-scala.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ~/Tools/
-wget http://downloads.typesafe.com/scala/2.11.6/scala-2.11.6.tgz && 
-tar xvzf scala-2.11.6.tgz 
-
