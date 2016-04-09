2231f70aabce723407f2e58c4898515628ab06ec
Timothy Washington <tim.washington@fundingcircle.com>
2016-04-09 13:37:24 -0700

    -- wip

--- profiles/core/etc/lein_profiles.clj
+++ profiles/core/etc/lein_profiles.clj
@@ -17,11 +17,32 @@
 
         :dependencies [[spyscope "0.1.5"]
                        [org.clojure/tools.namespace "0.2.10"]
+                       [com.cemerick/piggieback "0.2.1"]
                        [io.aviso/pretty "0.1.24"]
                        [im.chit/vinyasa "0.4.2"]
                        [alembic "0.3.2"]
                        [fipp "0.6.4"]
                        [leiningen "2.6.1"]]
+
+        #_:repl-options #_{:nrepl-middleware
+                       [cider.nrepl.middleware.apropos/wrap-apropos
+                        cider.nrepl.middleware.classpath/wrap-classpath
+                        cider.nrepl.middleware.complete/wrap-complete
+                        cider.nrepl.middleware.debug/wrap-debug
+                        cider.nrepl.middleware.format/wrap-format
+                        cider.nrepl.middleware.info/wrap-info
+                        cider.nrepl.middleware.inspect/wrap-inspect
+                        cider.nrepl.middleware.macroexpand/wrap-macroexpand
+                        cider.nrepl.middleware.ns/wrap-ns
+                        cider.nrepl.middleware.pprint/wrap-pprint
+                        cider.nrepl.middleware.pprint/wrap-pprint-fn
+                        cider.nrepl.middleware.refresh/wrap-refresh
+                        cider.nrepl.middleware.resource/wrap-resource
+                        cider.nrepl.middleware.stacktrace/wrap-stacktrace
+                        cider.nrepl.middleware.test/wrap-test
+                        cider.nrepl.middleware.trace/wrap-trace
+                        cider.nrepl.middleware.out/wrap-out
+                        cider.nrepl.middleware.undef/wrap-undef]}
 
         :injections [(require 'spyscope.core)
                      (require '[vinyasa.inject :as inject])
