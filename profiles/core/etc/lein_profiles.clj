bd3d8ea796c288c0b828eee47d351e4d064c04aa
Timothy Washington <tim.washington@fundingcircle.com>
2016-04-17 13:13:00 -0700

    -- cleaning up lein profiles

--- profiles/core/etc/lein_profiles.clj
+++ profiles/core/etc/lein_profiles.clj
@@ -1,17 +1,9 @@
-{:user {:plugins [[cider/cider-nrepl "0.12.0-SNAPSHOT"]
-                  [refactor-nrepl "2.2.0-SNAPSHOT"]
+{:user {:plugins [[cider/cider-nrepl "0.12.0"]
+                  [refactor-nrepl "2.2.0"]
                   [lein-ancient "0.6.8"]
-                  [codox "0.9.4"]
-                  [lein-ritz "0.7.0"]
-                  [lein-midje "3.2"]
-                  [lein-midje-doc "0.0.24"]
                   [lein-ring "0.9.7"]
                   [lein-try "0.4.3"]
-                  [lein-localrepo "0.5.3"]
                   [lein-cljsbuild "1.1.2"]
-                  [lein-clojars "0.9.1"]
-                  [lein-auto "0.1.2"]
-                  [lein-shell "0.5.0"]
                   [mvxcvi/whidbey "1.3.0"]
                   [venantius/ultra "0.4.1"]]
 
@@ -21,33 +13,10 @@
                        [spyscope "0.1.5"]
                        [io.aviso/pretty "0.1.24"]
                        [im.chit/vinyasa "0.4.2"]
-                       [alembic "0.3.2"]
-                       [fipp "0.6.4"]
                        [leiningen "2.6.1"]]
-
-        #_:repl-options #_{:nrepl-middleware
-                       [cider.nrepl.middleware.apropos/wrap-apropos
-                        cider.nrepl.middleware.classpath/wrap-classpath
-                        cider.nrepl.middleware.complete/wrap-complete
-                        cider.nrepl.middleware.debug/wrap-debug
-                        cider.nrepl.middleware.format/wrap-format
-                        cider.nrepl.middleware.info/wrap-info
-                        cider.nrepl.middleware.inspect/wrap-inspect
-                        cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                        cider.nrepl.middleware.ns/wrap-ns
-                        cider.nrepl.middleware.pprint/wrap-pprint
-                        cider.nrepl.middleware.pprint/wrap-pprint-fn
-                        cider.nrepl.middleware.refresh/wrap-refresh
-                        cider.nrepl.middleware.resource/wrap-resource
-                        cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                        cider.nrepl.middleware.test/wrap-test
-                        cider.nrepl.middleware.trace/wrap-trace
-                        cider.nrepl.middleware.out/wrap-out
-                        cider.nrepl.middleware.undef/wrap-undef]}
 
         :injections [(require 'spyscope.core)
                      (require '[vinyasa.inject :as inject])
-                     (require 'alembic.still)
                      (require 'io.aviso.repl)
 
                      (inject/in
@@ -56,8 +25,6 @@
                       ;; note that `:refer, :all and :exclude can be used
                       [vinyasa.inject :refer [inject [in inject-in]]]
                       [vinyasa.lein :refer [lein] :exclude [*project*]]
-                      [alembic.still :refer [distill load-project]]
-
                       [clojure.tools.namespace.repl :refer [refresh]]
 
                       ;; inject into clojure.core
