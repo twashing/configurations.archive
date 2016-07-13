df0cdfefd62abf71ce5ef776d01a2256acd66366
Timothy Washington <tim.washington@fundingcircle.com>
2016-07-13 15:36:51 -0700

    Updating lein plugins and including a missing lib that leiningen > pomegranate needs: org.apache.maven/maven-aether-provider

--- profiles/core/etc/lein_profiles.clj
+++ profiles/core/etc/lein_profiles.clj
@@ -1,19 +1,23 @@
-{:user {:plugins [[cider/cider-nrepl "0.12.0"]
+{:user {:plugins [[cider/cider-nrepl "0.13.0-snapshot"]
                   [refactor-nrepl "2.2.0"]
-                  [lein-ancient "0.6.8"]
+                  [lein-ancient "0.6.10"]
                   [lein-ring "0.9.7"]
                   [lein-try "0.4.3"]
-                  [lein-cljsbuild "1.1.2"]
+                  [lein-cljsbuild "1.1.3"]
+                  [lein-environ "1.0.3"]
                   [mvxcvi/whidbey "1.3.0"]
                   [venantius/ultra "0.4.1"]]
 
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [org.clojure/tools.namespace "0.2.10"]
-                       [com.cemerick/piggieback "0.2.1"]
                        [spyscope "0.1.5"]
-                       [io.aviso/pretty "0.1.24"]
-                       [im.chit/vinyasa "0.4.2"]
-                       [leiningen "2.6.1"]]
+                       [io.aviso/pretty "0.1.27"]
+                       [im.chit/vinyasa "0.4.7"]
+                       [leiningen "2.6.1" :exclusions [slingshot
+                                                       org.apache.maven.wagon/wagon-provider-api
+                                                       org.codehaus.plexus/plexus-utils
+                                                       commons-logging]]
+                       [org.apache.maven/maven-aether-provider "3.0.5"]]
 
         :injections [(require 'spyscope.core)
                      (require '[vinyasa.inject :as inject])
