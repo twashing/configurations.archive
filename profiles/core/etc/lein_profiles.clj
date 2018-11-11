e5e60ce610e0edf10473898c1cdf89c0ca836bf6
Timothy Washington <twashing@interruptsoftware.com>
2018-11-10 16:17:33 -0800

    Cleaning up config Pulling in zsh

--- profiles/core/etc/lein_profiles.clj
+++ profiles/core/etc/lein_profiles.clj
@@ -1,67 +1,12 @@
-{}
-
-#_{:user {:plugins [#_[cider/cider-nrepl "0.14.0"]
-                  [cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [refactor-nrepl "2.2.0"]
-                  [lein-cljfmt "0.5.6"]
-                  [lein-ancient "0.6.10"]]
-        :dependencies [[spyscope "0.1.5"]]
-        :injections [(require 'spyscope.core)]
-        }}
-
-#_{:user {:plugins [[cider/cider-nrepl "0.13.0-snapshot"]
-                  [refactor-nrepl "2.2.0"]
-                    
-                  [lein-ring "0.9.7"]
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [im.chit/lucid.core.inject "1.3.13"]]
+        :injections [(use 'lucid.core.inject)
+                     (inject '[clojure.core
+                               [clojure.pprint pprint]
+                               [clojure.java.javadoc javadoc]
+                               [clojure.reflect reflect]
+                               [clojure.repl apropos dir doc find-doc pst source]])]
+        :plugins [[refactor-nrepl "2.4.0"]
+                  [cider/cider-nrepl "0.18.0"]
                   [lein-try "0.4.3"]
-                  [lein-cljsbuild "1.1.3"]
-                  [lein-environ "1.0.3"]
-                  [mvxcvi/whidbey "1.3.0"]
-                  [venantius/ultra "0.4.1"]]
-
-          :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [org.clojure/tools.namespace "0.2.10"]
-                       [spyscope "0.1.5"]
-                       [io.aviso/pretty "0.1.27"]
-                       [im.chit/vinyasa "0.4.7"
-                        :exclusions [org.apache.maven/maven-aether-provider
-                                     org.codehaus.plexus/plexus-utils
-                                     org.apache.maven/maven-model
-                                     org.apache.maven/maven-model-builder]]
-                       [leiningen "2.6.1"
-                        :exclusions [slingshot
-                                     org.apache.maven.wagon/wagon-provider-api
-                                     org.codehaus.plexus/plexus-utils
-                                     org.apache.maven/maven-model
-                                     org.apache.maven/maven-model-builder
-                                     commons-logging
-                                     slingshot
-                                     org.codehaus.jackson/jackson-core-asl]]
-                       [org.apache.maven/maven-aether-provider "3.0.5"]
-                       [org.apache.maven/maven-model "3.0.5"]]
-
-        :injections [(require 'spyscope.core)
-                     (require '[vinyasa.inject :as inject])
-                     (require 'io.aviso.repl)
-
-                     (inject/in
-
-                      ;; the default injected namespace is `.`
-                      ;; note that `:refer, :all and :exclude can be used
-                      [vinyasa.inject :refer [inject [in inject-in]]]
-                      [vinyasa.lein :refer [lein] :exclude [*project*]]
-                      [clojure.tools.namespace.repl :refer [refresh]]
-
-                      ;; inject into clojure.core
-                      clojure.core
-                      [vinyasa.reflection .> .? .* .% .%> .& .>ns .>var]
-
-                      ;; inject into clojure.core with prefix
-                      clojure.core >
-                      [clojure.pprint pprint]
-                      [clojure.java.shell sh])
-
-                     (alter-var-root #'clojure.main/repl-caught
-                                     (constantly @#'io.aviso.repl/pretty-pst))
-                     (alter-var-root #'clojure.repl/pst
-                                     (constantly @#'io.aviso.repl/pretty-pst))]}}
+                  [lein-ancient "0.6.15"]]}}
